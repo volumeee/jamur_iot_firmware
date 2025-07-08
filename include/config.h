f8ba672a16fa2c920b14516b87b394c4806cb109
Bagus Erwanto <mr.volumee@gmail.com>
2025-07-08 20:55:33 +0700

    fix: update firmware version to v22

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 // ==      FILE KONFIGURASI PROYEK JAMUR IOT               ==
 // ==========================================================
 
-#define FIRMWARE_VERSION "v21"
+#define FIRMWARE_VERSION "v22"
 
 // --- KONFIGURASI PERANGKAT KERAS LOKAL (Layar & Tombol) ---
 #define LCD_ADDRESS 0x27
