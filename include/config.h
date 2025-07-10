78a7e777701eeb2ecbf06c155a5e3dc1c74c1ae7
Bagus Erwanto <mr.volumee@gmail.com>
2025-07-10 22:36:52 +0700

    fix: perbarui versi firmware ke v25.9

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 // ==      FILE KONFIGURASI PROYEK JAMUR IOT               ==
 // ==========================================================
 
-#define FIRMWARE_VERSION "v25.8"
+#define FIRMWARE_VERSION "v25.9"
 
 // --- KONFIGURASI PERANGKAT KERAS LOKAL (Layar & Tombol) ---
 #define LCD_ADDRESS 0x27
