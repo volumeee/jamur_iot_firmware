aa1ba9d3a5f8eedb0040558ed7bc5621a900cade
Bagus Erwanto <mr.volumee@gmail.com>
2025-07-15 19:37:36 +0700

    fix: perbarui versi firmware ke v27.4

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 // ==      FILE KONFIGURASI PROYEK JAMUR IOT               ==
 // ==========================================================
 
-#define FIRMWARE_VERSION "v27.3"
+#define FIRMWARE_VERSION "v27.4"
 
 // --- KONFIGURASI PERANGKAT KERAS LOKAL (Layar & Tombol) ---
 #define LCD_ADDRESS 0x27
