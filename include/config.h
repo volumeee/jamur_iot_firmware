b300bba556e4622da3e44771e8d1afdd17a38d2c
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2025-07-15 12:40:34 +0700

    Update config.h

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 // ==      FILE KONFIGURASI PROYEK JAMUR IOT               ==
 // ==========================================================
 
-#define FIRMWARE_VERSION "v27.2"
+#define FIRMWARE_VERSION "v27.3"
 
 // --- KONFIGURASI PERANGKAT KERAS LOKAL (Layar & Tombol) ---
 #define LCD_ADDRESS 0x27
